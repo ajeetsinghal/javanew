5d1b55e115ffcb0b5ac62ad4818e1ac8553b4d45
root <root@ip-172-31-30-53.us-east-2.compute.internal>
2021-07-03 15:06:53 +0000

    mtestknnn

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{
 public static void main(String args[]){
-System.out.println("Welcome to AKS Cloud Learning delhi");
+System.out.println("Welcome to AKS Clouninxinninciodnvonvood Learning delhi");
 }
 }
