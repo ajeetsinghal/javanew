4730c76e0bf24770470af6e971ecf89aea3ac4d6
root <root@ip-172-31-30-53.us-east-2.compute.internal>
2021-07-03 14:50:32 +0000

    new changessss

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-    class Test{  
-        public static void main(String args[]){  
-         System.out.println("Hello Java");  
+class Test{  
+public static void main(String args[]){  
+System.out.println("Hello Java progaram");  
         }  
-    }  
+}  
